36f3d0faa72f87a67c95fb15e3f8956dc1d7b14d
Cyrus Najmabadi <cyrusn@microsoft.com>
2012-12-11 12:00:42 -0800

    Adding hash combine function.

--- src/prototype/Hash.ts
+++ src/prototype/Hash.ts
@@ -31,7 +31,7 @@ class Hash {
         var index = start;
         while (numberOfCharsLeft >= 2) {
             var c1 = key[index];
-            var c2 = key[index + 1]; 
+            var c2 = key[index + 1];
 
             var k = c1 | (c2 << 16);
 
@@ -68,10 +68,11 @@ class Hash {
         var m = 0x5bd1e995;
         var r = 24;
 
-        // Initialize the hash to a 'random' value
         var start = 0;
         var len = key.length;
         var numberOfCharsLeft = len;
+
+        // Initialize the hash to a 'random' value.
         var h = (0 ^ numberOfCharsLeft);
 
         // Mix 4 bytes at a time into the hash.  NOTE: 4 bytes is two chars, so we iterate
@@ -111,12 +112,12 @@ class Hash {
     }
 
     private static primes =
-        [ 3, 7, 11, 17, 23, 29, 37, 47, 59, 71, 89, 107, 131, 163, 197, 239, 293, 353, 431, 521,
+        [3, 7, 11, 17, 23, 29, 37, 47, 59, 71, 89, 107, 131, 163, 197, 239, 293, 353, 431, 521,
           631, 761, 919, 1103, 1327, 1597, 1931, 2333, 2801, 3371, 4049, 4861, 5839, 7013, 8419,
-          10103, 12143, 14591, 17519, 21023, 25229, 30293, 36353, 43627, 52361, 62851, 75431, 
+          10103, 12143, 14591, 17519, 21023, 25229, 30293, 36353, 43627, 52361, 62851, 75431,
           90523, 108631, 130363, 156437, 187751, 225307, 270371, 324449, 389357, 467237, 560689,
           672827, 807403, 968897, 1162687, 1395263, 1674319, 2009191, 2411033, 2893249, 3471899,
-          4166287, 4999559, 5999471, 7199369 ];
+          4166287, 4999559, 5999471, 7199369];
 
     public static getPrime(min: number): number {
         for (var i = 0; i < primes.length; i++) {
@@ -135,5 +136,9 @@ class Hash {
             return 2146435069;
         }
         return getPrime(num);
+    }
+
+    public static combine(newKey: number, currentKey: number): number {
+        return (currentKey * 0xA5555529) + newKey;
     }
 }
