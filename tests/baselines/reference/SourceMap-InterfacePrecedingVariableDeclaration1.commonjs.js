8ee49312a80702d16a11fdf186b92a6d4660da70
Cyrus Najmabadi <cyrusn@microsoft.com>
2013-01-15 12:28:17 -0800

    Use JSON2 to actually create the source map output instead of manually merging strings.
    This ensures we create proper JSON (which we are not doing in some cases where values
    contain the quote character).

--- tests/baselines/reference/SourceMap-InterfacePrecedingVariableDeclaration1.commonjs.js
+++ tests/baselines/reference/SourceMap-InterfacePrecedingVariableDeclaration1.commonjs.js
@@ -1,4 +1,4 @@
 var x = 0;
 //@ sourceMappingURL=0.js.map
 ////[0.js.map]
-{"version":3,"file":"0.js","sources":["0.ts"],"names":[""],"mappings":"AAKA,IAAI,CAAC,GAAG,CAAC,CAAC"}
+{"version":3,"file":"0.js","sources":["0.ts"],"names":[],"mappings":"AAKA,IAAI,CAAC,GAAG,CAAC,CAAC"}
