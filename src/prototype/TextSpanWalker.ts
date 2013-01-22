3f71aab7935632dbb03317a45d0c46e5272f13a4
Cyrus Najmabadi <cyrusn@microsoft.com>
2013-01-22 13:46:57 -0800

    Adding a simple walker to allow one to walk a portion of a tree.

---
+++ src/prototype/TextSpanWalker.ts
@@ -0,0 +1,25 @@
+///<reference path='SyntaxWalker.generated.ts' />
+
+class TextSpanWalker extends SyntaxWalker {
+    private position: number = 0;
+
+    constructor(private textSpan: TextSpan) {
+        super();
+    }
+
+    public visitToken(token: ISyntaxToken): void {
+        this.position += token.fullWidth();
+    }
+
+    public visitNode(node: SyntaxNode): void {
+        var nodeSpan = new TextSpan(this.position, node.fullWidth());
+
+        if (nodeSpan.intersectsWithTextSpan(this.textSpan)) {
+            node.accept(this);
+        }
+        else {
+            // We're skipping the node, so update our position accordingly.
+            this.position += node.fullWidth();
+        }
+    }
+}
