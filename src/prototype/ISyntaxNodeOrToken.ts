d1dac6b2b6c2545adf44b877061dec1fdffad3aa
Mohamed Hegazy <mhegazy@microsoft.com>
2013-01-23 15:39:43 -0800

    Hook outlining

--- src/prototype/ISyntaxNodeOrToken.ts
+++ src/prototype/ISyntaxNodeOrToken.ts
@@ -9,6 +9,11 @@ interface ISyntaxNodeOrToken extends ISyntaxElement {
     leadingTrivia(): ISyntaxTriviaList;
     trailingTrivia(): ISyntaxTriviaList;
 
+    width(): number;
+
+    leadingTriviaWidth(): number;
+    trailingTriviaWidth(): number;
+    
     withLeadingTrivia(leadingTrivia: ISyntaxTriviaList): ISyntaxNodeOrToken;
     withTrailingTrivia(trailingTrivia: ISyntaxTriviaList): ISyntaxNodeOrToken;
 
