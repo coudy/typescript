6da9662906e9b0f3de2468f84f11e62de89ccfa2
Sheetal Nandi <shkamat@microsoft.com>
2013-02-18 16:46:03 -0800

    Fixed 740: Using function returntype from external module results in error when the type is both value and type (608088)

---
+++ tests/cases/compiler/returnTypeOfFunctionFromExternalModule.ts
@@ -0,0 +1,42 @@
+// @Filename: system.d.ts
+export interface ValueCallback {
+    (value: any): any;
+}
+
+export interface EventCallback {
+    (value: any): void;
+}
+
+export interface ErrorCallback {
+    (error: any): any;
+}
+
+export interface ProgressCallback {
+    (progress: any): any;
+}
+
+export interface CancelCallback {
+    (): void;
+}
+
+export interface Promise {
+    then(success?: ValueCallback, error?: ErrorCallback, progress?: ProgressCallback): Promise;
+    done(success?: ValueCallback, error?: ErrorCallback, progress?: ProgressCallback): void;
+    cancel(): void;
+}
+
+export declare var Promise: {
+    prototype: Promise;
+    as(value: any): Promise;
+}
+
+
+// @Filename: test.ts
+import system = module('system');
+
+function foo(): void {
+    var p: system.Promise;
+    p = system.Promise.as(10);
+}
+function bar(): system.Promise {
+}
