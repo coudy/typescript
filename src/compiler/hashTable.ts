ad62256d57f93f17e3f1b545f2286add6c5030f0
Cyrus Najmabadi <cyrusn@microsoft.com>
2013-03-15 15:48:53 -0700

    Clean up StringHashTable.

--- src/compiler/hashTable.ts
+++ src/compiler/hashTable.ts
@@ -36,89 +36,95 @@ module TypeScript {
         getAllKeys(): string[];
         add(key: string, data): bool;
         addOrUpdate(key: string, data): bool;
-        map(fn: (k: string, v, c) => void , context): void;
-        every(fn: (k: string, v, c) => bool, context): bool;
-        some(fn: (k: string, v, c) => bool, context): bool;
+        map(fn: (k: string, value: any, context: any) => void , context: any): void;
+        every(fn: (k: string, value: any, context: any) => void , context: any): bool;
+        some(fn: (k: string, value: any, context: any) => void , context: any): bool;
         count(): number;
         lookup(key: string): any;
     }
 
     export class StringHashTable implements IHashTable {
         public itemCount = 0;
-        public table = <any>(<any> new BlockIntrinsics());
+        public table: any = new BlockIntrinsics();
 
-        public getAllKeys(): string[]{
+        public getAllKeys(): string[] {
             var result: string[] = [];
+
             for (var k in this.table) {
-                if (this.table[k] != undefined) {
-                    result[result.length] = k;
+                if (this.table[k] !== undefined) {
+                    result.push(k);
                 }
             }
+
             return result;
         }
 
         public add(key: string, data): bool {
-            if (this.table[key] != undefined) {
+            if (this.table[key] !== undefined) {
                 return false;
             }
+
             this.table[key] = data;
             this.itemCount++;
             return true;
         }
 
         public addOrUpdate(key: string, data): bool {
-            if (this.table[key] != undefined) {
+            if (this.table[key] !== undefined) {
                 this.table[key] = data;
                 return false;
             }
+
             this.table[key] = data;
             this.itemCount++;
             return true;
         }
 
-        public map(fn: (k: string, v, c) => void , context) {
+        public map(fn: (k: string, value: any, context: any) => void , context: any) {
             for (var k in this.table) {
                 var data = this.table[k];
-                if (data != undefined) {
+
+                if (data !== undefined) {
                     fn(k, this.table[k], context);
                 }
             }
         }
 
-        public every(fn: (k: string, v, c) => bool, context) {
+        public every(fn: (k: string, value: any, context: any) => void , context: any) {
             for (var k in this.table) {
                 var data = this.table[k];
-                if (data != undefined) {
+
+                if (data !== undefined) {
                     if (!fn(k, this.table[k], context)) {
                         return false;
                     }
                 }
             }
+
             return true;
         }
 
-        public some(fn: (k: string, v, c) => bool, context) {
+        public some(fn: (k: string, value: any, context: any) => void , context: any) {
             for (var k in this.table) {
                 var data = this.table[k];
-                if (data != undefined) {
+
+                if (data !== undefined) {
                     if (fn(k, this.table[k], context)) {
                         return true;
                     }
                 }
             }
+
             return false;
         }
 
-        public count(): number { return this.itemCount; }
+        public count(): number {
+            return this.itemCount;
+        }
 
         public lookup(key: string) {
             var data = this.table[key];
-            if (data != undefined) {
-                return data;
-            }
-            else {
-                return (null);
-            }
+            return data === undefined ? null : data;
         }
     }
 
