a27a52e76704ca625ae964f4c1080ae1520b1c60
Joe Pamer <jopamer@microsoft.com>
2013-03-17 20:45:02 -0700

    Update compiler perf harness

--- tests/cases/webharness/wholecompiler.ts
+++ tests/cases/webharness/wholecompiler.ts
@@ -5040,112 +5040,6 @@ module TypeScript {
 // limitations under the License.
 //
 
-module TypeScript {
-    export class AstLogger {
-
-        constructor (public logger: ILogger) { }
-
-        public logScript(script: TypeScript.Script): void {
-            this.logLinemap(script.locationInfo.lineMap);
-
-            var stack: AST[]= [];
-
-            var pre = (cur: TypeScript.AST, parent: TypeScript.AST) => {
-                stack.push(cur);
-                var indent = (stack.length - 1) * 2;
-                this.logComments(script, cur.preComments, indent);
-                this.logNode(script, cur, indent);
-                this.logComments(script, cur.postComments, indent);
-                return cur;
-            }
-
-            var post = (cur: TypeScript.AST, parent: TypeScript.AST) => {
-                stack.pop();
-                return cur;
-            }
-
-            TypeScript.getAstWalkerFactory().walk(script, pre, post);
-        }
-
-
-        public logNode(script: TypeScript.Script, cur: TypeScript.AST, indent: number) {
-            var msg = this.addPadding("", indent, "| ", true);
-
-            msg = msg.concat("+ " + cur.treeViewLabel());
-            msg = this.addPadding(msg, 70, " ", false);
-
-            msg = msg + this.addLineColumn(script, cur.minChar);
-            msg = this.addPadding(msg, 80, " ", false);
-
-            msg = msg + "=> ";
-            msg = msg + this.addLineColumn(script, cur.limChar);
-            msg = this.addPadding(msg, 102, " ", false);
-
-            msg = msg.concat("[" + this.addPadding(cur.minChar.toString(), 1, " ", true) + ", " + this.addPadding(cur.limChar.toString(), 1, " ", true) + "]");
-
-            msg = this.addPadding(msg, 115, " ", false);
-            msg = msg.concat("sym=" + (<any>cur).sym);
-
-            msg = this.addPadding(msg, 135, " ", false);
-            msg = msg.concat("type=" + (cur.type === null ? "null" : cur.type.getTypeName()));
-            this.logger.log(msg);
-        }
-
-        private logComments(script: TypeScript.Script, comments: TypeScript.AST[], indent: number) {
-            if (comments == null)
-                return;
-
-            for (var i = 0; i < comments.length; i++) {
-                this.logNode(script, comments[i], indent);
-            }
-        }
-
-        public logLinemap(linemap: number[]) {
-            var result = "[";
-            for (var i = 0; i < linemap.length; i++) {
-                if (i > 0) {
-                    result += ",";
-                }
-
-                result += linemap[i];
-            }
-            result += "]";
-            this.logger.log("linemap: " + result);
-        }
-
-        private addPadding(s: string, targetLength: number, paddingString: string, leftPadding: bool): string {
-            var result = (leftPadding ? "" : s);
-            for (var i = s.length; i < targetLength; i++) {
-                result = result + paddingString;
-            }
-            result = result + (leftPadding ? s : "");
-            return result;
-        }
-
-        private addLineColumn(script: TypeScript.Script, position: number): string {
-            // just for calling getSourceLineColFromMap
-            var lineInfo = { line: -1, col: -1 };
-            TypeScript.getZeroBasedSourceLineColFromMap(lineInfo, position, script.locationInfo.lineMap);
-
-            return "(" + lineInfo.line + ", " + lineInfo.col + ")";
-        }
-    }
-}
-﻿//﻿
-// Copyright (c) Microsoft Corporation.  All rights reserved.
-// 
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//   http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-//
-
 ///<reference path='typescript.ts' />
 
 module TypeScript {
@@ -25730,13 +25624,8 @@ module TypeScript {
 ///<reference path='typescript.ts' />
 
 module TypeScript {
-
     export class IncrementalParser {
-        
-        private astLogger: AstLogger;
-
-        constructor (private logger: TypeScript.ILogger) {
-            this.astLogger = new AstLogger(this.logger);
+        constructor(private logger: TypeScript.ILogger) {
         }
 
         //
@@ -25913,14 +25802,6 @@ module TypeScript {
             var lineMap1 = script.locationInfo.lineMap;
             var lineMap2 = partial.locationInfo.lineMap;
 
-            if (this.logger.information()) {
-                this.logger.log("lineMap1 (before):");
-                this.astLogger.logLinemap(lineMap1);
-                this.logger.log("lineMap2 (quick parse):");
-                this.astLogger.logLinemap(lineMap2);
-                this.logger.log("EditRange=" + editRange);
-            }
-
             // Skip entries < minChar
             var i1 = 1; // lineMap[0] is always 0.
             var i2 = 1; // lineMap[0] is always 0.
@@ -25973,11 +25854,6 @@ module TypeScript {
                 for (; i2 < len2; i2++) {
                     lineMap1.push(lineMap2[i2] + editRange.span().start());
                 }
-            }
-
-            if (this.logger.information()) {
-                this.logger.log("lineMap1 (after merge):");
-                this.astLogger.logLinemap(lineMap1);
             }
         }
 
@@ -68124,7 +68000,6 @@ module TypeScript {
 ///<reference path='astWalker.ts' />
 ///<reference path='astWalkerCallback.ts' />
 ///<reference path='astPath.ts' />
-///<reference path='astLogger.ts' />
 ///<reference path='binder.ts' />
 ///<reference path='base64.ts' />
 ///<reference path='sourceMapping.ts' />
