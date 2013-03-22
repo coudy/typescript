1e04934bcca5e490955048f3f4422290ab78d984
Cyrus Najmabadi <cyrusn@microsoft.com>
2013-03-21 21:56:04 -0700

    Removing unused function.

--- src/compiler/flags.ts
+++ src/compiler/flags.ts
@@ -263,23 +263,4 @@ module TypeScript {
     export var moduleGenTarget: ModuleGenTarget = ModuleGenTarget.Synchronous;
 
     export var optimizeModuleCodeGen = true;
-
-    export function flagsToString(e, flags: number): string {
-        var builder = "";
-        for (var i = 1; i < (1 << 31) ; i = i << 1) {
-            if ((flags & i) != 0) {
-                for (var k in e) {
-                    if (e[k] == i) {
-                        if (builder.length > 0) {
-                            builder += "|";
-                        }
-                        builder += k;
-                        break;
-                    }
-                }
-            }
-        }
-        return builder;
-    }
-
 }
