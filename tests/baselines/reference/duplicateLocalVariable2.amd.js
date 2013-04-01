fc7249c6f8cb054f299809b5525de0000aa93913
Joe Pamer <jopamer@microsoft.com>
2013-03-31 18:27:54 -0700

    baseline update

--- tests/baselines/reference/duplicateLocalVariable2.amd.js
+++ tests/baselines/reference/duplicateLocalVariable2.amd.js
@@ -9,24 +9,11 @@ define(["require", "exports"], function(require, exports) {
     })();
     exports.TestCase = TestCase;    
     var TestRunner = (function () {
-<<<<<<< HEAD
         function TestRunner() { }
         TestRunner.arrayCompare = function arrayCompare(arg1, arg2) {
             return false;
         };
         TestRunner.prototype.addTest = function (test) {
-=======
-        function TestRunner() {
-            this.tests = [];
-        }
-        TestRunner.arrayCompare = function arrayCompare(arg1, arg2) {
-            return (arg1.every(function (val, index) {
-                return val === arg2[index];
-            }));
-        };
-        TestRunner.prototype.addTest = function (test) {
-            this.tests.push(test);
->>>>>>> Avoid unnecessary resolution of generic type arguments (marking them as "in resolution" made this a no-op)
         };
         return TestRunner;
     })();
@@ -34,22 +21,10 @@ define(["require", "exports"], function(require, exports) {
     exports.tests = (function () {
         var testRunner = new TestRunner();
         testRunner.addTest(new TestCase("Check UTF8 encoding", function () {
-<<<<<<< HEAD
             var fb;
             fb.writeUtf8Bom();
             var chars = [
                 0x0054
-=======
-            var fb = new FileManager.FileBuffer(20);
-            fb.writeUtf8Bom();
-            var chars = [
-                0x0054, 
-                0x00E8, 
-                0x1D23, 
-                0x2020, 
-                0x000D, 
-                0x000A
->>>>>>> Avoid unnecessary resolution of generic type arguments (marking them as "in resolution" made this a no-op)
             ];
             for(var i in chars) {
                 fb.writeUtf8CodePoint(chars[i]);
@@ -60,24 +35,7 @@ define(["require", "exports"], function(require, exports) {
                 bytes.push(fb.readByte());
             }
             var expected = [
-<<<<<<< HEAD
                 0xEF
-=======
-                0xEF, 
-                0xBB, 
-                0xBF, 
-                0x54, 
-                0xC3, 
-                0xA8, 
-                0xE1, 
-                0xB4, 
-                0xA3, 
-                0xE2, 
-                0x80, 
-                0xA0, 
-                0x0D, 
-                0x0A
->>>>>>> Avoid unnecessary resolution of generic type arguments (marking them as "in resolution" made this a no-op)
             ];
             return TestRunner.arrayCompare(bytes, expected);
         }));
--- tests/baselines/reference/duplicateLocalVariable2.commonjs.js
+++ tests/baselines/reference/duplicateLocalVariable2.commonjs.js
@@ -8,24 +8,11 @@ var TestCase = (function () {
 })();
 exports.TestCase = TestCase;
 var TestRunner = (function () {
-<<<<<<< HEAD
     function TestRunner() { }
     TestRunner.arrayCompare = function arrayCompare(arg1, arg2) {
         return false;
     };
     TestRunner.prototype.addTest = function (test) {
-=======
-    function TestRunner() {
-        this.tests = [];
-    }
-    TestRunner.arrayCompare = function arrayCompare(arg1, arg2) {
-        return (arg1.every(function (val, index) {
-            return val === arg2[index];
-        }));
-    };
-    TestRunner.prototype.addTest = function (test) {
-        this.tests.push(test);
->>>>>>> Avoid unnecessary resolution of generic type arguments (marking them as "in resolution" made this a no-op)
     };
     return TestRunner;
 })();
@@ -33,22 +20,10 @@ exports.TestRunner = TestRunner;
 exports.tests = (function () {
     var testRunner = new TestRunner();
     testRunner.addTest(new TestCase("Check UTF8 encoding", function () {
-<<<<<<< HEAD
         var fb;
         fb.writeUtf8Bom();
         var chars = [
             0x0054
-=======
-        var fb = new FileManager.FileBuffer(20);
-        fb.writeUtf8Bom();
-        var chars = [
-            0x0054, 
-            0x00E8, 
-            0x1D23, 
-            0x2020, 
-            0x000D, 
-            0x000A
->>>>>>> Avoid unnecessary resolution of generic type arguments (marking them as "in resolution" made this a no-op)
         ];
         for(var i in chars) {
             fb.writeUtf8CodePoint(chars[i]);
@@ -59,24 +34,7 @@ exports.tests = (function () {
             bytes.push(fb.readByte());
         }
         var expected = [
-<<<<<<< HEAD
             0xEF
-=======
-            0xEF, 
-            0xBB, 
-            0xBF, 
-            0x54, 
-            0xC3, 
-            0xA8, 
-            0xE1, 
-            0xB4, 
-            0xA3, 
-            0xE2, 
-            0x80, 
-            0xA0, 
-            0x0D, 
-            0x0A
->>>>>>> Avoid unnecessary resolution of generic type arguments (marking them as "in resolution" made this a no-op)
         ];
         return TestRunner.arrayCompare(bytes, expected);
     }));
