9965b58fcb2bb6f59095528ede103ec01b347491
Dan Quirk <danquirk@microsoft.com>
2013-04-11 19:33:56 -0700

    Move passing old fourslash tests to current suite, delete already moved tests

--- tests/cases/fourslash_old/debuggerStatementIndent.ts
+++
@@ -1,7 +0,0 @@
-/// <reference path='fourslash.ts' />
-//// if (false) {
-////     debugger;
-//// /**/
-goTo.marker();
-edit.insert('}');
-verify.smartIndentLevelIs(0);
--- tests/cases/fourslash_old/moduleIndent.ts
+++
@@ -1,7 +0,0 @@
-/// <reference path="fourslash.ts"/>
-//// 
-
-goTo.bof();
-edit.insert("module M {\n");
-// indentation on newline after "module {"
-verify.smartIndentLevelIs(1);
