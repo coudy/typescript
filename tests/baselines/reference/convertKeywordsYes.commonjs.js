51ece61772bf94b86f2d06743a5f9b1ac58af46f
unknown <pvanbren@pvanbren-001.redmond.corp.microsoft.com>
2013-04-24 16:22:57 -0700

    Updating keywords test to include more keywords in more locations

--- tests/baselines/reference/convertKeywordsYes.commonjs.js
+++ tests/baselines/reference/convertKeywordsYes.commonjs.js
@@ -1,145 +1,350 @@
-var implements;
-var let;
-var private;
-var public;
-var yield;
-var interface;
-var package;
-var protected;
-var static;
-var big = {
-    break: 0,
-    do: 0,
-    instanceof: 0,
-    typeof: 0,
-    case: 0,
-    else: 0,
-    new: 0,
-    var: 0,
-    catch: 0,
-    finally: 0,
-    return: 0,
-    void: 0,
-    continue: 0,
-    for: 0,
-    switch: 0,
-    while: 0,
-    debugger: 0,
-    function: 0,
-    this: 0,
-    with: 0,
-    default: 0,
-    if: 0,
-    throw: 0,
-    delete: 0,
-    in: 0,
-    try: 0,
-    enum: 0,
-    extends: 0,
-    super: 0,
-    const: 0,
-    export: 0,
-    import: 0,
+var constructor = 0;
+var any = 0;
+var bool = 0;
+var boolean = 0;
+var implements = 0;
+var interface = 0;
+var let = 0;
+var module = 0;
+var number = 0;
+var package = 0;
+var private = 0;
+var protected = 0;
+var public = 0;
+var set = 0;
+var static = 0;
+var string = 0;
+var get = 0;
+var yield = 0;
+var declare = 0;
+function bigGeneric(c, a, b, b2, i, i2, l, m, n, p, p2, p3, p4, s, s2, s3, g, y, d) {
+}
+var bigObject = {
+    constructor: 0,
+    any: 0,
+    bool: 0,
+    boolean: 0,
     implements: 0,
-    let: 0,
-    private: 0,
-    public: 0,
-    yield: 0,
     interface: 0,
+    let: 0,
+    module: 0,
+    number: 0,
     package: 0,
+    private: 0,
     protected: 0,
-    static: 0
+    public: 0,
+    set: 0,
+    static: 0,
+    string: 0,
+    get: 0,
+    yield: 0,
+    break: 0,
+    case: 0,
+    catch: 0,
+    class: 0,
+    continue: 0,
+    const: 0,
+    debugger: 0,
+    declare: 0,
+    default: 0,
+    delete: 0,
+    do: 0,
+    else: 0,
+    enum: 0,
+    export: 0,
+    extends: 0,
+    false: 0,
+    finally: 0,
+    for: 0,
+    function: 0,
+    if: 0,
+    import: 0,
+    in: 0,
+    instanceof: 0,
+    new: 0,
+    null: 0,
+    return: 0,
+    super: 0,
+    switch: 0,
+    this: 0,
+    throw: 0,
+    true: 0,
+    try: 0,
+    typeof: 0,
+    var: 0,
+    void: 0,
+    while: 0,
+    with: 0
 };
 var bigClass = (function () {
     function bigClass() {
+        this.constructor = 0;
+        this.any = 0;
+        this.bool = 0;
+        this.boolean = 0;
+        this.implements = 0;
+        this.interface = 0;
+        this.let = 0;
+        this.module = 0;
+        this.number = 0;
+        this.package = 0;
+        this.private = 0;
+        this.protected = 0;
+        this.public = 0;
+        this.set = 0;
+        this.static = 0;
+        this.string = 0;
+        this.get = 0;
+        this.yield = 0;
+        this.break = 0;
+        this.case = 0;
+        this.catch = 0;
+        this.class = 0;
+        this.continue = 0;
+        this.const = 0;
+        this.debugger = 0;
+        this.declare = 0;
+        this.default = 0;
+        this.delete = 0;
+        this.do = 0;
+        this.else = 0;
+        this.enum = 0;
+        this.export = 0;
+        this.extends = 0;
+        this.false = 0;
+        this.finally = 0;
+        this.for = 0;
+        this.function = 0;
+        this.if = 0;
+        this.import = 0;
+        this.in = 0;
+        this.instanceof = 0;
+        this.new = 0;
+        this.null = 0;
+        this.return = 0;
+        this.super = 0;
+        this.switch = 0;
+        this.this = 0;
+        this.throw = 0;
+        this.true = 0;
+        this.try = 0;
+        this.typeof = 0;
+        this.var = 0;
+        this.void = 0;
+        this.while = 0;
+        this.with = 0;
     }
     return bigClass;
 })();
 var bigEnum;
 (function (bigEnum) {
     bigEnum._map = [];
-    bigEnum._map[0] = "break";
-    bigEnum.break = 0;
-    bigEnum._map[1] = "do";
-    bigEnum.do = 1;
-    bigEnum._map[2] = "instanceof";
-    bigEnum.instanceof = 2;
-    bigEnum._map[3] = "typeof";
-    bigEnum.typeof = 3;
-    bigEnum._map[4] = "case";
-    bigEnum.case = 4;
-    bigEnum._map[5] = "else";
-    bigEnum.else = 5;
-    bigEnum._map[6] = "new";
-    bigEnum.new = 6;
-    bigEnum._map[7] = "var";
-    bigEnum.var = 7;
-    bigEnum._map[8] = "catch";
-    bigEnum.catch = 8;
-    bigEnum._map[9] = "finally";
-    bigEnum.finally = 9;
-    bigEnum._map[10] = "return";
-    bigEnum.return = 10;
-    bigEnum._map[11] = "void";
-    bigEnum.void = 11;
-    bigEnum._map[12] = "continue";
-    bigEnum.continue = 12;
-    bigEnum._map[13] = "for";
-    bigEnum.for = 13;
-    bigEnum._map[14] = "switch";
-    bigEnum.switch = 14;
-    bigEnum._map[15] = "while";
-    bigEnum.while = 15;
-    bigEnum._map[16] = "debugger";
-    bigEnum.debugger = 16;
-    bigEnum._map[17] = "function";
-    bigEnum.function = 17;
-    bigEnum._map[18] = "this";
-    bigEnum.this = 18;
-    bigEnum._map[19] = "with";
-    bigEnum.with = 19;
-    bigEnum._map[20] = "default";
-    bigEnum.default = 20;
-    bigEnum._map[21] = "if";
-    bigEnum.if = 21;
-    bigEnum._map[22] = "throw";
-    bigEnum.throw = 22;
-    bigEnum._map[23] = "delete";
-    bigEnum.delete = 23;
-    bigEnum._map[24] = "in";
-    bigEnum.in = 24;
-    bigEnum._map[25] = "try";
-    bigEnum.try = 25;
-    bigEnum._map[26] = "class";
-    bigEnum.class = 26;
-    bigEnum._map[27] = "enum";
-    bigEnum.enum = 27;
-    bigEnum._map[28] = "extends";
-    bigEnum.extends = 28;
-    bigEnum._map[29] = "super";
-    bigEnum.super = 29;
-    bigEnum._map[30] = "const";
-    bigEnum.const = 30;
+    bigEnum._map[0] = "constructor";
+    bigEnum.constructor = 0;
+    bigEnum._map[1] = "any";
+    bigEnum.any = 1;
+    bigEnum._map[2] = "bool";
+    bigEnum.bool = 2;
+    bigEnum._map[3] = "boolean";
+    bigEnum.boolean = 3;
+    bigEnum._map[4] = "implements";
+    bigEnum.implements = 4;
+    bigEnum._map[5] = "interface";
+    bigEnum.interface = 5;
+    bigEnum._map[6] = "let";
+    bigEnum.let = 6;
+    bigEnum._map[7] = "module";
+    bigEnum.module = 7;
+    bigEnum._map[8] = "number";
+    bigEnum.number = 8;
+    bigEnum._map[9] = "package";
+    bigEnum.package = 9;
+    bigEnum._map[10] = "private";
+    bigEnum.private = 10;
+    bigEnum._map[11] = "protected";
+    bigEnum.protected = 11;
+    bigEnum._map[12] = "public";
+    bigEnum.public = 12;
+    bigEnum._map[13] = "set";
+    bigEnum.set = 13;
+    bigEnum._map[14] = "static";
+    bigEnum.static = 14;
+    bigEnum._map[15] = "string";
+    bigEnum.string = 15;
+    bigEnum._map[16] = "get";
+    bigEnum.get = 16;
+    bigEnum._map[17] = "yield";
+    bigEnum.yield = 17;
+    bigEnum._map[18] = "break";
+    bigEnum.break = 18;
+    bigEnum._map[19] = "case";
+    bigEnum.case = 19;
+    bigEnum._map[20] = "catch";
+    bigEnum.catch = 20;
+    bigEnum._map[21] = "class";
+    bigEnum.class = 21;
+    bigEnum._map[22] = "continue";
+    bigEnum.continue = 22;
+    bigEnum._map[23] = "const";
+    bigEnum.const = 23;
+    bigEnum._map[24] = "debugger";
+    bigEnum.debugger = 24;
+    bigEnum._map[25] = "declare";
+    bigEnum.declare = 25;
+    bigEnum._map[26] = "default";
+    bigEnum.default = 26;
+    bigEnum._map[27] = "delete";
+    bigEnum.delete = 27;
+    bigEnum._map[28] = "do";
+    bigEnum.do = 28;
+    bigEnum._map[29] = "else";
+    bigEnum.else = 29;
+    bigEnum._map[30] = "enum";
+    bigEnum.enum = 30;
     bigEnum._map[31] = "export";
     bigEnum.export = 31;
-    bigEnum._map[32] = "import";
-    bigEnum.import = 32;
-    bigEnum._map[33] = "implements";
-    bigEnum.implements = 33;
-    bigEnum._map[34] = "let";
-    bigEnum.let = 34;
-    bigEnum._map[35] = "private";
-    bigEnum.private = 35;
-    bigEnum._map[36] = "public";
-    bigEnum.public = 36;
-    bigEnum._map[37] = "yield";
-    bigEnum.yield = 37;
-    bigEnum._map[38] = "interface";
-    bigEnum.interface = 38;
-    bigEnum._map[39] = "package";
-    bigEnum.package = 39;
-    bigEnum._map[40] = "protected";
-    bigEnum.protected = 40;
-    bigEnum._map[41] = "static";
-    bigEnum.static = 41;
+    bigEnum._map[32] = "extends";
+    bigEnum.extends = 32;
+    bigEnum._map[33] = "false";
+    bigEnum.false = 33;
+    bigEnum._map[34] = "finally";
+    bigEnum.finally = 34;
+    bigEnum._map[35] = "for";
+    bigEnum.for = 35;
+    bigEnum._map[36] = "function";
+    bigEnum.function = 36;
+    bigEnum._map[37] = "if";
+    bigEnum.if = 37;
+    bigEnum._map[38] = "import";
+    bigEnum.import = 38;
+    bigEnum._map[39] = "in";
+    bigEnum.in = 39;
+    bigEnum._map[40] = "instanceof";
+    bigEnum.instanceof = 40;
+    bigEnum._map[41] = "new";
+    bigEnum.new = 41;
+    bigEnum._map[42] = "null";
+    bigEnum.null = 42;
+    bigEnum._map[43] = "return";
+    bigEnum.return = 43;
+    bigEnum._map[44] = "super";
+    bigEnum.super = 44;
+    bigEnum._map[45] = "switch";
+    bigEnum.switch = 45;
+    bigEnum._map[46] = "this";
+    bigEnum.this = 46;
+    bigEnum._map[47] = "throw";
+    bigEnum.throw = 47;
+    bigEnum._map[48] = "true";
+    bigEnum.true = 48;
+    bigEnum._map[49] = "try";
+    bigEnum.try = 49;
+    bigEnum._map[50] = "typeof";
+    bigEnum.typeof = 50;
+    bigEnum._map[51] = "var";
+    bigEnum.var = 51;
+    bigEnum._map[52] = "void";
+    bigEnum.void = 52;
+    bigEnum._map[53] = "while";
+    bigEnum.while = 53;
+    bigEnum._map[54] = "with";
+    bigEnum.with = 54;
 })(bigEnum || (bigEnum = {}));
+var bigModule;
+(function (bigModule) {
+    var constructor = (function () {
+        function constructor() {
+        }
+        return constructor;
+    })();
+    var any = (function () {
+        function any() {
+        }
+        return any;
+    })();
+    var bool = (function () {
+        function bool() {
+        }
+        return bool;
+    })();
+    var boolean = (function () {
+        function boolean() {
+        }
+        return boolean;
+    })();
+    var implements = (function () {
+        function implements() {
+        }
+        return implements;
+    })();
+    var interface = (function () {
+        function interface() {
+        }
+        return interface;
+    })();
+    var let = (function () {
+        function let() {
+        }
+        return let;
+    })();
+    var module = (function () {
+        function module() {
+        }
+        return module;
+    })();
+    var number = (function () {
+        function number() {
+        }
+        return number;
+    })();
+    var package = (function () {
+        function package() {
+        }
+        return package;
+    })();
+    var private = (function () {
+        function private() {
+        }
+        return private;
+    })();
+    var protected = (function () {
+        function protected() {
+        }
+        return protected;
+    })();
+    var public = (function () {
+        function public() {
+        }
+        return public;
+    })();
+    var set = (function () {
+        function set() {
+        }
+        return set;
+    })();
+    var static = (function () {
+        function static() {
+        }
+        return static;
+    })();
+    var string = (function () {
+        function string() {
+        }
+        return string;
+    })();
+    var get = (function () {
+        function get() {
+        }
+        return get;
+    })();
+    var yield = (function () {
+        function yield() {
+        }
+        return yield;
+    })();
+    var declare = (function () {
+        function declare() {
+        }
+        return declare;
+    })();
+})(bigModule || (bigModule = {}));
