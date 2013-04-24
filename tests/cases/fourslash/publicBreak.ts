1bbab4589cc95ffb86a26958e64173c8a99c0c3a
Ryan Cavanaugh <ryanca@microsoft.com>
2013-04-24 16:13:31 -0700

    More regression tests

---
+++ tests/cases/fourslash/publicBreak.ts
@@ -0,0 +1,8 @@
+/// <reference path="fourslash.ts" />
+
+//// public break;
+//// /**/
+
+goTo.marker();
+// Bug 673655: exception in incremental update when file contains "public break;" at top-level
+// edit.insert(' ');
---
+++ tests/cases/fourslash/quickInfoOfLablledForStatementIterator.ts
@@ -0,0 +1,7 @@
+/// <reference path="fourslash.ts" />
+
+//// label1: for(var /**/i = 0; i < 1; i++) { }
+
+goTo.marker();
+// Bug 673526: No quick info on iterator variable in labelled for loop statement
+// verify.quickInfoExists();
