7a2ae0996703afc85c47171c149cbb96abc71880
Ryan Cavanaugh <ryanca@microsoft.com>
2013-04-24 09:25:19 -0700

    Add new regression test

---
+++ tests/cases/fourslash/removeDeclareInModule.ts
@@ -0,0 +1,14 @@
+/// <reference path="fourslash.ts" />
+
+//// /**/declare module Foo {
+////     function a(): void {}
+//// }
+//// 
+//// Foo.a();
+
+goTo.marker();
+edit.deleteAtCaret('declare '.length);
+// Bug 672580: Removing 'declare' from module doesn't catch access of non-exported member
+// verify.numberOfErrorsInCurrentFile(1); // Expected 1: 'a' is not exported
+verify.numberOfErrorsInCurrentFile(0);
+
