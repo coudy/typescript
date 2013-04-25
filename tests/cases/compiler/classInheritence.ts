833baaeaf2d8c48ab9e7005b27f781fbec05b89b
Ryan Cavanaugh <ryanca@microsoft.com>
2013-04-25 16:29:45 -0700

    Unblock tests for fixed bugs

--- tests/cases/compiler/classInheritence.ts
+++ tests/cases/compiler/classInheritence.ts
@@ -1,3 +1,2 @@
-// Bug 17512: Out of stack space in slightly more complicated extends-self inheritance case
-//class B extends A { }
-//class A extends A { }
+class B extends A { }
+class A extends A { }
