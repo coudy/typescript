148df7191d1e4c6652bdf474d951a52fa9f0d158
Joe Pamer <jopamer@microsoft.com>
2013-04-29 11:29:30 -0700

    Revert "More pathing fixups"
    This reverts commit 429c05f644a6294cf62d8c8a77d0a3a381d3d4d0.

--- tests/baselines/reference/exportedUsingPrivate.amd.js
+++ tests/baselines/reference/exportedUsingPrivate.amd.js
@@ -1,21 +1,16 @@
 define(["require", "exports"], function(require, exports) {
     var C1 = (function () {
-        function C1() {
-        }
+        function C1() { }
         return C1;
-    })();
-
+    })();    
     var C2 = (function () {
-        function C2() {
-        }
+        function C2() { }
         C2.prototype.test = function () {
             return true;
         };
         return C2;
-    })();
-
+    })();    
     ;
-
     exports.e;
     exports.f;
     exports.g;
