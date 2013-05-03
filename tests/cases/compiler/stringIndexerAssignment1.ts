5e9c861c98e6457a06801e00f049363463b81268
Dan Quirk <danquirk@microsoft.com>
2013-05-03 12:41:58 -0700

    Delete flakey test

--- tests/cases/compiler/stringIndexerAssignment1.ts
+++
@@ -1,8 +0,0 @@
-interface IPerson {
-   firstName: string;
-   lastName: string;
-}
-var persons: { [id: string]: IPerson; } = {
-   "p1": { firstName: "F1", lastName: "L1" },
-   "p2": { firstName: "F2" }
-};
