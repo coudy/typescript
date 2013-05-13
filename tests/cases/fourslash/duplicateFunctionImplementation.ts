9bad21811b9302056acdf0bc96e416a700ae8ad5
Ryan Cavanaugh <ryanca@microsoft.com>
2013-05-13 09:41:48 -0700

    Mark unblocked test

--- tests/cases/fourslash/duplicateFunctionImplementation.ts
+++ tests/cases/fourslash/duplicateFunctionImplementation.ts
@@ -7,5 +7,4 @@
 //// function foo<T>(x: T): T { return null; }
 
 goTo.marker();
-// Bug 681081: Duplicated function implementation gets treated as an overload for error reporting
 edit.insert("x: string");
