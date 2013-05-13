0813ba86bb514ac0e2278f72e4e588c737859814
Ryan Cavanaugh <ryanca@microsoft.com>
2013-05-13 14:37:10 -0700

    Test is unblocked

--- tests/cases/fourslash/removeExportedClassFromReopenedModule.ts
+++ tests/cases/fourslash/removeExportedClassFromReopenedModule.ts
@@ -11,6 +11,6 @@ goTo.marker('1');
 edit.deleteAtCaret('export class c { }'.length);
 
 goTo.eof();
-// Bug 674557: Removing exported class from reopened internal module doesn't always flag consumption sites as errors
-//edit.insert("new multiM.c();");
-//verify.numberOfErrorsInCurrentFile(1);
+
+edit.insert("new multiM.c();");
+verify.numberOfErrorsInCurrentFile(1);
