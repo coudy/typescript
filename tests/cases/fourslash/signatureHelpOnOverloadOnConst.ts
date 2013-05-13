b65455ef767d80f2e9240d5d8e3879c8c5618e96
Ryan Cavanaugh <ryanca@microsoft.com>
2013-05-13 14:16:56 -0700

    Re-enable test for checking sig help on const overloads

--- tests/cases/fourslash/signatureHelpOnOverloadOnConst.ts
+++ tests/cases/fourslash/signatureHelpOnOverloadOnConst.ts
@@ -5,10 +5,22 @@
 ////function x1(z: string);
 ////function x1(a: any) {
 ////}
+////
+////x1(''/*1*/);
+////x1('hi'/*2*/);
+////x1('bye'/*3*/);
 
-////x1(''/**/);
-
-goTo.marker();
+goTo.marker('1');
 verify.signatureHelpCountIs(3);
 verify.currentParameterHelpArgumentNameIs("z");
 verify.currentParameterSpanIs("z: string");
+
+goTo.marker('2');
+verify.signatureHelpCountIs(3);
+verify.currentParameterHelpArgumentNameIs("x");
+verify.currentParameterSpanIs("x: 'hi'");
+
+goTo.marker('3');
+verify.signatureHelpCountIs(3);
+verify.currentParameterHelpArgumentNameIs("y");
+verify.currentParameterSpanIs("y: 'bye'");
