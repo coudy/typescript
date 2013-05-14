aa7c670aa519a353ec4796e77700bf1f3dbc8a36
Ryan Cavanaugh <ryanca@microsoft.com>
2013-05-13 17:55:00 -0700

    Test is extra-blocked now

--- tests/cases/fourslash/toggleDuplicateFunctionDeclaration.ts
+++ tests/cases/fourslash/toggleDuplicateFunctionDeclaration.ts
@@ -6,8 +6,8 @@
 var funcDecl = 'declare function D();';
 
 goTo.bof();
-edit.insert(funcDecl);
+// Bug 673244: Adding duplicate function declaration of class name doesn't result in incremental error
+// edit.insert(funcDecl);
 
 goTo.bof();
-// Bug 673244: Adding duplicate function declaration of class name doesn't result in incremental error
 // edit.deleteAtCaret(funcDecl.length);
