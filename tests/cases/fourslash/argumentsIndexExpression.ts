d38f2abeb0d22a478ec7dc82130ccc856e1cf324
Ryan Cavanaugh <ryanca@microsoft.com>
2013-05-14 12:40:10 -0700

    New regression test for getting symbol info on 'arguments' index expr.

---
+++ tests/cases/fourslash/argumentsIndexExpression.ts
@@ -0,0 +1,9 @@
+/// <reference path="fourslash.ts" />
+
+//// function f() {
+////     var x = /**/arguments[0];
+//// }
+
+goTo.marker();
+// Bug 692109: Exception getting quickinfo here
+// verify.quickInfoExists();
