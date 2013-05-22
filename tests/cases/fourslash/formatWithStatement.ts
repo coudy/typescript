c88c4fd3101d3ef42798f3cf8cfb89144805180e
Shane Li <v-lshane@microsoft.com>
2013-05-22 19:00:03 +0800

    Fourslash conversion on formatting.

---
+++ tests/cases/fourslash/formatWithStatement.ts
@@ -0,0 +1,37 @@
+/// <reference path='fourslash.ts' />
+
+////with /*1*/(foo.bar)
+////
+////   {/*2*/
+////
+////     }/*3*/
+////
+////with (bar.blah)/*4*/
+////{/*5*/
+////}/*6*/
+
+format.setOption("PlaceOpenBraceOnNewLineForControlBlocks", false);
+format.document();
+goTo.marker("1");
+// Bug 660913
+//verify.currentLineContentIs("with (foo.bar) {");
+verify.currentLineContentIs("with (foo.bar)");
+goTo.marker("3");
+verify.currentLineContentIs("}");
+goTo.marker("4");
+// Bug 660913
+//verify.currentLineContentIs("with (bar.blah) {");
+verify.currentLineContentIs("with (bar.blah)");
+goTo.marker("6");
+verify.currentLineContentIs("}");
+
+format.setOption("PlaceOpenBraceOnNewLineForControlBlocks", true);
+format.document();
+goTo.marker("1");
+verify.currentLineContentIs("with (foo.bar)");
+goTo.marker("2");
+verify.currentLineContentIs("{");
+goTo.marker("4");
+verify.currentLineContentIs("with (bar.blah)");
+goTo.marker("5");
+verify.currentLineContentIs("{");
