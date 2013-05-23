ed8ee659a480fabb0e067cc7d162c0ad7117595f
Dan Quirk <danquirk@microsoft.com>
2013-05-23 16:08:17 -0700

    Trivial fix for previous test

--- tests/cases/fourslash/underscoreTypings1.ts
+++ tests/cases/fourslash/underscoreTypings1.ts
@@ -3,28 +3,28 @@
 ////interface Iterator<T, U> {
 ////    (value: T, index: any, list: any): U;
 ////}
-
+////
 ////interface WrappedArray<T> {
 ////    map<U>(iterator: Iterator<T, U>, context?: any): U[];
 ////}
-
+////
 ////interface Underscore {
 ////    <T>(list: T[]): WrappedArray<T>;
 ////    map<T, U>(list: T[], iterator: Iterator<T, U>, context?: any): U[];
 ////}
-
+////
 ////declare var _: Underscore;
-
+////
 ////var a: string[];
 ////var b/*1*/ = _.map(a, x/*2*/ => x.length);    // Type any[], should be number[]
 ////var c/*3*/ = _(a).map(x/*4*/ => x.length);
 ////var d/*5*/ = a.map(x/*6*/ => x.length);
-
+////
 ////var aa: any[];
 ////var bb/*7*/ = _.map(aa, x/*8*/ => x.length);
 ////var cc/*9*/ = _(aa).map(x/*10*/ => x.length);  // Error, could not select overload
 ////var dd/*11*/ = aa.map(x/*12*/ => x.length);     // Error, could not select overload
-
+////
 ////var e = a.map(x => x./*13*/
 
 goTo.marker('1');
