d52ca51f63eb5fe62a1a32045e6befa8a6abec7f
Cyrus Najmabadi <cyrusn@microsoft.com>
2013-05-24 13:46:15 -0700

    Make our hashtable type generic.

--- src/compiler/core/hashTable.ts
+++ src/compiler/core/hashTable.ts
@@ -3,22 +3,22 @@
 module TypeScript.Collections {
     export var DefaultHashTableCapacity = 256;
 
-    class HashTableEntry {
-        constructor(public Key: any,
-                    public Value: any,
+    class HashTableEntry<TKey, TValue> {
+        constructor(public Key: TKey,
+                    public Value: TValue,
                     public HashCode: number,
-                    public Next: HashTableEntry) {
+                    public Next: HashTableEntry<TKey,TValue>) {
         }
     }
 
-    export class HashTable {
+    export class HashTable<TKey, TValue> {
         // TODO: figure out why the type checker dosn't like "HashTableEntry[]" here.
-        private entries: HashTableEntry[] = [];
+        private entries: HashTableEntry<TKey, TValue>[] = [];
         private count: number = 0;
 
         constructor(capacity: number,
-                    private hash: (k: any) => number,
-                    private equals: (k1: any, k2: any) => boolean) {
+                    private hash: (k: TKey) => number,
+                    private equals: (k1: TKey, k2: TKey) => boolean) {
             var size = Hash.getPrime(capacity);
             this.hash = hash;
             this.equals = equals;
@@ -26,31 +26,31 @@ module TypeScript.Collections {
         }
 
         // Maps 'key' to 'value' in this table.  Does not throw if 'key' is already in the table.
-        public set (key: any, value: any) {
+        public set (key: TKey, value: TValue): void {
             this.addOrSet(key, value, /*throwOnExistingEntry:*/ false);
         }
 
         // Maps 'key' to 'value' in this table.  Throws if 'key' is already in the table.
-        public add(key: any, value: any) {
+        public add(key: TKey, value: TValue): void {
             this.addOrSet(key, value, /*throwOnExistingEntry:*/ true);
         }
 
-        public containsKey(key: any): boolean {
+        public containsKey(key: TKey): boolean {
             var hashCode = this.computeHashCode(key);
             var entry = this.findEntry(key, hashCode);
             return entry !== null;
         }
 
-        public get (key: any): any {
+        public get (key: TKey): TValue {
             var hashCode = this.computeHashCode(key);
             var entry = this.findEntry(key, hashCode);
 
             return entry === null ? null : entry.Value;
         }
 
-        private computeHashCode(key: any): number {
+        private computeHashCode(key: TKey): number {
             var hashCode = this.hash === null
-                ? key.hashCode()
+                ? (<any>key).hashCode()
                 : this.hash(key);
 
             hashCode = hashCode & 0x7FFFFFFF;
@@ -59,7 +59,7 @@ module TypeScript.Collections {
             return hashCode;
         }
 
-        private addOrSet(key: any, value: any, throwOnExistingEntry: boolean) {
+        private addOrSet(key: TKey, value: TValue, throwOnExistingEntry: boolean) {
             // Compute the hash for this key.  Also ensure that it's non negative.
             var hashCode = this.computeHashCode(key);
 
@@ -77,7 +77,7 @@ module TypeScript.Collections {
             return this.addEntry(key, value, hashCode);
         }
 
-        private findEntry(key: any, hashCode: number) /*: HashTableEntry*/ {
+        private findEntry(key: TKey, hashCode: number): HashTableEntry<TKey, TValue> {
             for (var e = this.entries[hashCode % this.entries.length]; e !== null; e = e.Next) {
                 if (e.HashCode === hashCode) {
                     var equals = this.equals === null
@@ -93,10 +93,10 @@ module TypeScript.Collections {
             return null;
         }
 
-        private addEntry(key: any, value: any, hashCode: number): any {
+        private addEntry(key: TKey, value: TValue, hashCode: number): any {
             var index = hashCode % this.entries.length;
 
-            var e = new HashTableEntry(key, value, hashCode, this.entries[index]);
+            var e = new HashTableEntry<TKey,TValue>(key, value, hashCode, this.entries[index]);
 
             this.entries[index] = e;
 
@@ -138,7 +138,7 @@ module TypeScript.Collections {
             var newSize = Hash.expandPrime(this.entries.length);
 
             var oldEntries = this.entries;
-            var newEntries: HashTableEntry[] = ArrayUtilities.createArray(newSize, null);
+            var newEntries: HashTableEntry<TKey,TValue>[] = ArrayUtilities.createArray(newSize, null);
 
             this.entries = newEntries;
 
@@ -158,10 +158,10 @@ module TypeScript.Collections {
         }
     }
 
-    export function createHashTable(capacity: number = DefaultHashTableCapacity,
-        hash: (k: any) => number = null,
-        equals: (k1: any, k2: any) => boolean = null): HashTable {
-        return new HashTable(capacity, hash, equals);
+    export function createHashTable<TKey,TValue>(capacity: number = DefaultHashTableCapacity,
+        hash: (k: TKey) => number = null,
+        equals: (k1: TKey, k2: TKey) => boolean = null): HashTable<TKey,TValue> {
+        return new HashTable<TKey,TValue>(capacity, hash, equals);
     }
 
     var currentHashCode = 1;
