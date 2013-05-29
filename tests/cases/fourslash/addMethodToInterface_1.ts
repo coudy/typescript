e82478ba713f7a2fa59a2ed93fa0464b9288cc27
Ryan Cavanaugh <ryanca@microsoft.com>
2013-05-28 18:59:17 -0700

    Remove spurious test content lines

--- tests/cases/fourslash/addMethodToInterface_1.ts
+++ tests/cases/fourslash/addMethodToInterface_1.ts
@@ -1,4 +1,4 @@
-/// <reference path="../fourslash.ts" />
+/// <reference path="fourslash.ts" />
 
 //// interface Comparable<T> {
 //// }
@@ -10,15 +10,15 @@
 edit.disableFormatting();
 diagnostics.validateTypesAtPositions(59,114,74,76,84);
 
-////   1: interface Comparable<T> {
-////    :  |->-> go here
-////   2: }
-////   3: interface Comparer {
+//   1: interface Comparable<T> {
+//    :  |->-> go here
+//   2: }
+//   3: interface Comparer {
 goTo.position(26);
 
-////   1: interface Comparable<T> {
-////    :  |->-> insert "    compareTo(): number;\n"
-////   2: }
-////   3: interface Comparer {
+//   1: interface Comparable<T> {
+//    :  |->-> insert "    compareTo(): number;\n"
+//   2: }
+//   3: interface Comparer {
 edit.insert("    compareTo(): number;\n");
 //diagnostics.validateTypesAtPositions(168,84,53,118,22);
