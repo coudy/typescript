f94fe8079d36e80574079c3925fc32e68efacc9d
Dan Quirk <danquirk@microsoft.com>
2013-05-30 12:44:37 -0700

    Updating clodule tests

--- tests/baselines/reference/augmentedTypesClass.amd.js
+++ tests/baselines/reference/augmentedTypesClass.amd.js
@@ -1,44 +1,20 @@
-define(["require", "exports"], function(require, exports) {
-    var c5 = (function () {
-        function c5() {
-        }
-        c5.prototype.foo = function () {
-        };
-        return c5;
-    })();
+var c1 = (function () {
+    function c1() {
+    }
+    c1.prototype.foo = function () {
+    };
+    return c1;
+})();
+var c1 = 1;
 
-    var c5a = (function () {
-        function c5a() {
-        }
-        c5a.prototype.foo = function () {
-        };
-        return c5a;
-    })();
-
-    var c5a;
-    (function (c5a) {
-        var y = 2;
-    })(c5a || (c5a = {}));
-
-    var c5b = (function () {
-        function c5b() {
-        }
-        c5b.prototype.foo = function () {
-        };
-        return c5b;
-    })();
-
-    var c5b;
-    (function (c5b) {
-        c5b.y = 2;
-    })(c5b || (c5b = {}));
-
-    var c5c = (function () {
-        function c5c() {
-        }
-        c5c.prototype.foo = function () {
-        };
-        return c5c;
-    })();
-    
-});
+var c4 = (function () {
+    function c4() {
+    }
+    c4.prototype.foo = function () {
+    };
+    return c4;
+})();
+var c4;
+(function (c4) {
+    c4[c4["One"] = 0] = "One";
+})(c4 || (c4 = {}));
