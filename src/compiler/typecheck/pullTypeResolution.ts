4f8c1d7de93d0e2838399d1ab95a5b80026c05a2
Jason Freeman <jfreeman@microsoft.com>
2013-06-03 12:04:42 -0700

    When resolving an enum, resolve it using resolveModuleDeclaration

--- src/compiler/typecheck/pullTypeResolution.ts
+++ src/compiler/typecheck/pullTypeResolution.ts
@@ -4775,6 +4775,7 @@ module TypeScript {
                     break;
                 case PullElementKind.DynamicModule:
                 case PullElementKind.Container:
+                case PullElementKind.Enum:
                     var moduleDecl = <ModuleDeclaration>this.semanticInfoChain.getASTForDecl(decl);
                     this.resolveModuleDeclaration(moduleDecl, context);
                     break;
