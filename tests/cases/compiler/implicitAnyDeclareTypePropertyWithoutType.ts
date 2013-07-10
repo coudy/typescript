1c8e4d8fa4cd6e756c252ecd9101aaa985c63d9a
Yui <t-kasuv@microsoft.com>
2013-07-10 12:41:54 -0700

    Merge disallow-implicit-any checking to the new typecheck system

--- tests/cases/compiler/implicitAnyDeclareTypePropertyWithoutType.ts
+++ tests/cases/compiler/implicitAnyDeclareTypePropertyWithoutType.ts
@@ -13,3 +13,5 @@ var x3: (x3: string, y3) => void ; // error at "y3"
 // this should not be an error
 var bar: { a: number; b: number };
 var foo: { littleC: C; c: string };
+var x4: new () => any;
+var x5: () => any;
