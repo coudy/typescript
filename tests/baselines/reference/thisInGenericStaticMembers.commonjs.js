8bae1060bb8fedfb6c0e43674075668dbbe1b4c0
paulb <paul.van.brenk@microsoft.com>
2013-07-10 09:32:25 -0700

    Added testcase for this calls in generic static members

---
+++ tests/baselines/reference/thisInGenericStaticMembers.commonjs.js
@@ -0,0 +1,25 @@
+var A = (function () {
+    function A() {
+    }
+    A.one = function (source, value) {
+        return source;
+    };
+
+    A.two = function (source) {
+        return this.one(source, 42);
+    };
+    return A;
+})();
+
+var B = (function () {
+    function B() {
+    }
+    B.one = function (source, value) {
+        return source;
+    };
+
+    B.two = function (source) {
+        return this.one(source, 42);
+    };
+    return B;
+})();
