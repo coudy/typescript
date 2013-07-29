f052595e222982d731e32fd1a18a3ce207fee2b1
Cyrus Najmabadi <cyrusn@microsoft.com>
2013-07-29 13:12:39 -0700

    Update test.

--- tests/cases/fourslash/mergedDeclarationsWithExportAssignment1.ts
+++ tests/cases/fourslash/mergedDeclarationsWithExportAssignment1.ts
@@ -12,7 +12,7 @@
 ////}
 ////import Foo/*1*/ = require('M');
 ////var z/*3*/ = new /*2*/Foo();
-////var r2/*5*/ = Foo./*4*/z;
+////var r2/*5*/ = Foo./*4*/x;
 
 goTo.marker('1');
 verify.quickInfoIs('{ x: number; new(): Foo; }');
@@ -21,15 +21,11 @@ goTo.marker('2');
 verify.completionListContains('Foo');
 
 goTo.marker('3');
-// BUG 713458
 verify.quickInfoIs('Foo');
-//verify.quickInfoIs('any');
 
 goTo.marker('4');
 verify.completionListContains('x');
 
 goTo.marker('5');
-// BUG 713458
-//verify.quickInfoIs('number');
-verify.quickInfoIs('any');
+verify.quickInfoIs('number');
 
