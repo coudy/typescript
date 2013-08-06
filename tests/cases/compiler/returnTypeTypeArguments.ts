056ffaeee89ce61313e155460c4510f9e943eff3
Paul van Brenk <paul.van.brenk@microsoft.com>
2013-08-06 10:48:00 -0700

    Adding test case for typearguments in return types

---
+++ tests/cases/compiler/returnTypeTypeArguments.ts
@@ -0,0 +1,39 @@
+class One<T>{
+    value: T;
+}
+class Two<T, U>{
+    value: T;
+    id: U;
+}
+class Three<T, U, V>{
+    value: T;
+    id: U;
+    name: V;
+}
+
+function A1(): One { return null; }
+function A2(): Two { return null; }
+function A3(): Three { return null; }
+
+function B1(): Two<number> { return null; }
+function B2(): Three<string> { return null; }
+function B3(): Three<string, number> { return null; }
+
+class C {
+    A1(): One { return null; }
+    A2(): Two { return null; }
+    A3(): Three { return null; }
+
+    B1(): Two<number> { return null; }
+    B2(): Three<string> { return null; }
+    B3(): Three<string, number> { return null; }
+}
+
+class D<T> {
+    A2(): Two<T> { return null; }
+    A3(): Three<T> { return null; }
+
+    B1(): Two<T> { return null; }
+    B2(): Three<T> { return null; }
+    B3(): Three<string, T> { return null; }
+}
