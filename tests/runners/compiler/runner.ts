1f20a07cbe939c8a633eb1ed98a36ab63048d3ef
Jason Freeman <jfreeman@microsoft.com>
2013-08-05 17:39:36 -0700

    Include dots and dashes when replacing full paths in error baselines

--- tests/runners/compiler/runner.ts
+++ tests/runners/compiler/runner.ts
@@ -93,7 +93,7 @@ class CompilerBaselineRunner extends RunnerBase {
                     } else {
                         // Certain errors result in full paths being reported, namely when types of external modules are involved
                         // we'll strip the full path and just report the filename
-                        var fullPath = /\w+:(\/|\\)(\w+|\/)*\.ts/g;
+                        var fullPath = /\w+:(\/|\\)([\w+\-\.]|\/)*\.ts/g;
                         var hasFullPath = errorDescriptionLocal.match(fullPath);
                         if (hasFullPath) {
                             hasFullPath.forEach(match => {
