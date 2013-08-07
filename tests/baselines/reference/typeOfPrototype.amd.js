a180a5188f9cf7dea648d2474d9cf4972a7c2e23
Paul van Brenk <paul.van.brenk@microsoft.com>
2013-08-07 14:47:10 -0700

    Unblocking test.

--- tests/baselines/reference/typeOfPrototype.amd.js
+++ tests/baselines/reference/typeOfPrototype.amd.js
@@ -1,4 +1,3 @@
-// bug 755692: Type of prototype should be same as constructor function
 var Foo = (function () {
     function Foo() {
         this.bar = 3;
--- tests/cases/compiler/typeOfPrototype.ts
+++ tests/cases/compiler/typeOfPrototype.ts
@@ -1,5 +1,3 @@
-// bug 755692: Type of prototype should be same as constructor function
-
 class Foo {
     bar = 3;
     static bar = '';
