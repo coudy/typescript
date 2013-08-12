c9ad78166bd2dbeb272b7a12ec7fac16b5d9b320
Sheetal Nandi <shkamat@microsoft.com>
2013-08-12 13:54:43 -0700

    Test case for privacy check of typeparameter referenced in constructor arguments

---
+++ tests/baselines/reference/privacyCheckOnTypeParameterReferenceInConstructorParameter.commonjs.js
@@ -0,0 +1,15 @@
+var A = (function () {
+    function A(callback) {
+        var child = new B(this);
+    }
+    return A;
+})();
+exports.A = A;
+
+var B = (function () {
+    function B(parent) {
+    }
+    return B;
+})();
+exports.B = B;
+
---
+++ tests/cases/compiler/privacyCheckOnTypeParameterReferenceInConstructorParameter.ts
@@ -0,0 +1,9 @@
+export class A<T1>{
+    constructor(callback: (self: A<T1>) => void) {
+        var child = new B(this);
+    }
+}
+
+export class B<T2> {
+    constructor(parent: T2) { }
+}
