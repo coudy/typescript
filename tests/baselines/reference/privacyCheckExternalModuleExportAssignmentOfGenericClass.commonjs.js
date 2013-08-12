02ab4ca35371016ff628a2b9746b5d9f71dd385d
Sheetal Nandi <shkamat@microsoft.com>
2013-08-12 13:33:18 -0700

    Test case with export assignment in external module with generic class

---
+++ tests/baselines/reference/privacyCheckExternalModuleExportAssignmentOfGenericClass.commonjs.js
@@ -0,0 +1,13 @@
+
+var Foo = (function () {
+    function Foo(a) {
+        this.a = a;
+    }
+    return Foo;
+})();
+module.exports = Foo;
+
+
+var Foo = require("privacyCheckExternalModuleExportAssignmentOfGenericClass_0");
+
+
