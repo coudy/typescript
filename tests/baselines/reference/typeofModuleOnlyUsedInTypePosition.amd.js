ed24f99e99fc8bbc138750ebb90cb1ce22c28f19
Sheetal Nandi <shkamat@microsoft.com>
2013-08-15 13:41:34 -0700

    Remove duplicate test case after merge from release-0.9.1.1

--- tests/baselines/reference/typeofModuleOnlyUsedInTypePosition.amd.js
+++
@@ -1,5 +0,0 @@
-define(["require", "exports", 'a'], function(require, exports, __a__) {
-    // this generated invalid javascript in AMD modules, the 'a' is added to the define.
-    var a = __a__;
-    var x;
-});
--- tests/baselines/reference/typeofModuleOnlyUsedInTypePosition.commonjs.js
+++
@@ -1,4 +0,0 @@
-// this generated invalid javascript in AMD modules, the 'a' is added to the define.
-var a = require('a');
-var x;
-
