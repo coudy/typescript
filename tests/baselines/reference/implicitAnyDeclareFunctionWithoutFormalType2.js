828fd4faaa59d9d6de2fe11ba8cb7fc6fc1f8c16
Paul van Brenk <paul.van.brenk@microsoft.com>
2013-08-23 16:02:07 -0700

    Add testcase for implicit any and declare files

---
+++ tests/baselines/reference/implicitAnyDeclareFunctionWithoutFormalType2.js
@@ -0,0 +1,24 @@
+//// [implicitAnyDeclareFunctionWithoutFormalType2.js]
+// generates function fn1(): number;
+function fn1() {
+    var x;
+    return x;
+}
+
+// generates function fn2(): any;
+function fn2() {
+    var x;
+    return x;
+}
+
+// generates function fn3();
+function fn3() {
+    var x;
+    return x;
+}
+
+
+////[implicitAnyDeclareFunctionWithoutFormalType2.d.ts]
+declare function fn1(): number;
+declare function fn2(): any;
+declare function fn3(): any;
