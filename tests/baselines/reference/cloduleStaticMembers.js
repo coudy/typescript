1babb9325742f60102b88fc5a58c290b7fe24fb5
Paul van Brenk <paul.van.brenk@microsoft.com>
2013-08-23 16:24:22 -0700

    Add test case for statics in clodules

---
+++ tests/baselines/reference/cloduleStaticMembers.js
@@ -0,0 +1,15 @@
+var Clod = (function () {
+    function Clod() {
+    }
+    Clod.x = 10;
+    Clod.y = 10;
+    return Clod;
+})();
+var Clod;
+(function (Clod) {
+    var p = Clod.x;
+    var q = x;
+
+    var s = Clod.y;
+    var t = y;
+})(Clod || (Clod = {}));
