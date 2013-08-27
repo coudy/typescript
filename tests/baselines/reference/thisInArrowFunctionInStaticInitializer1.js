26218569fb29ba16bcbad3679f5ed5463e99e821
Cyrus Najmabadi <cyrusn@microsoft.com>
2013-08-27 16:03:33 -0700

    Adding tests.

---
+++ tests/baselines/reference/thisInArrowFunctionInStaticInitializer1.js
@@ -0,0 +1,13 @@
+function log(a) {
+}
+
+var Vector = (function () {
+    function Vector() {
+        var _this = this;
+    }
+    Vector.foo = function () {
+        // 'this' should not be available in a static initializer.
+        log(_this);
+    };
+    return Vector;
+})();
---
+++ tests/baselines/reference/unqualifiedCallToClassStatic1.js
@@ -0,0 +1,9 @@
+var Vector = (function () {
+    function Vector() {
+    }
+    Vector.foo = function () {
+        // 'foo' cannot be called in an unqualified manner.
+        foo();
+    };
+    return Vector;
+})();
