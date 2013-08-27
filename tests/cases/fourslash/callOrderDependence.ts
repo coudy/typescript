b5ffe6fef6bad333cbe7e3e08af7c3ea01ad292a
Ryan Cavanaugh <ryanca@microsoft.com>
2013-08-27 10:44:20 -0700

    Remove console logging from test

--- tests/cases/fourslash/callOrderDependence.ts
+++ tests/cases/fourslash/callOrderDependence.ts
@@ -7,5 +7,4 @@
 edit.replace(0,0,"function foo(bar) {\n    b\n}\n");
 goTo.position(27);
 FourSlash.currentTestState.getCompletionListAtCaret().entries
-  .forEach(entry =>
-    console.log(FourSlash.currentTestState.getCompletionEntryDetails(entry.name)));
+  .forEach(entry => FourSlash.currentTestState.getCompletionEntryDetails(entry.name));
