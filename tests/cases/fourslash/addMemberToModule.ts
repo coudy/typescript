a9032459870bc79aa1b22be7610b792ff28b4996
Ryan Cavanaugh <ryanca@microsoft.com>
2013-09-03 12:43:44 -0700

    Tests for new LS bugs

---
+++ tests/cases/fourslash/addMemberToModule.ts
@@ -0,0 +1,21 @@
+/// <reference path="fourslash.ts" />
+
+////module A {
+////    /*var*/
+////}
+////module A/*check*/ {
+////    var p;
+////}
+
+diagnostics.setEditValidation(IncrementalEditValidation.SyntacticOnly);
+
+goTo.marker('check');
+verify.quickInfoExists();
+
+goTo.marker('var');
+edit.insert('var o;');
+
+goTo.marker('check');
+// Bug 771751: Crash here
+// verify.quickInfoExists();
+
