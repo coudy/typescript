1bed94c1172b3e058f7aaf9c0ec4f46c9d9cc7ed
Sheetal Nandi <shkamat@microsoft.com>
2013-09-06 13:12:27 -0700

    Test case for error to define ambient external module in another external module

---
+++ tests/baselines/reference/ambientExternalModuleInAnotherExternalModule.js
@@ -0,0 +1,13 @@
+define(["require", "exports", "ext"], function(require, exports, __ext__) {
+    var D = (function () {
+        function D() {
+        }
+        return D;
+    })();
+    
+
+    // Cannot resolve this ext module reference
+    var ext = "ext";
+    var x = ext;
+    return D;
+});
