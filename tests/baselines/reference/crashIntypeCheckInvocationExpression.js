f859283b84124a85e6036490c24d432105dad1d6
Mohamed Hegazy <mhegazy@microsoft.com>
2013-09-12 12:28:34 -0700

    add unit tests for crashes

---
+++ tests/baselines/reference/crashIntypeCheckInvocationExpression.js
@@ -0,0 +1,10 @@
+define(["require", "exports"], function(require, exports) {
+    var nake;
+    function doCompile(fileset, moduleType) {
+        return undefined;
+    }
+    exports.compileServer = task(function () {
+        var folder = path.join(), fileset = nake.fileSetSync(folder);
+        return doCompile(fileset, moduleType);
+    });
+});
---
+++ tests/baselines/reference/crashIntypeCheckObjectCreationExpression.js
@@ -0,0 +1,13 @@
+define(["require", "exports"], function(require, exports) {
+    var BuildWorkspaceService = (function () {
+        function BuildWorkspaceService() {
+        }
+        BuildWorkspaceService.prototype.injectRequestService = function (service) {
+            this.injectBuildService(new X(service));
+        };
+        BuildWorkspaceService.prototype.injectBuildService = function (service) {
+        };
+        return BuildWorkspaceService;
+    })();
+    exports.BuildWorkspaceService = BuildWorkspaceService;
+});
