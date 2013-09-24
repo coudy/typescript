28f624b84b4e096b064cfbdf2f22e7b9577cb09a
Ryan Cavanaugh <ryanca@microsoft.com>
2013-09-24 09:27:03 -0700

    Remove deleted baselines

--- tests/baselines/reference/constructorParameterInitializerThis.js
+++
@@ -1,8 +0,0 @@
-// this should not compiler, this reference in in
-// constructor parameter initializer not allowed
-var Foo = (function () {
-    function Foo(x) {
-        if (typeof x === "undefined") { x = this.y; }
-    }
-    return Foo;
-})();
