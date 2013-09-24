1b9986b6e86b7303858c945b6bdf7c6ec260fc29
Cyrus Najmabadi <cyrusn@microsoft.com>
2013-09-23 20:01:23 -0700

    No need to normalize the filename slashes here.  THey'll already have been normalized above.

--- src/services/pullLanguageService.ts
+++ src/services/pullLanguageService.ts
@@ -978,7 +978,7 @@ module Services {
                 if (path.ast().nodeType() === TypeScript.NodeType.FunctionDeclaration) {
                     var funcDecl = <TypeScript.FunctionDeclaration>(path.ast());
                     if (symbol && symbol.kind != TypeScript.PullElementKind.Property) {
-                        var signatureInfo = TypeScript.PullHelpers.getSignatureForFuncDecl(funcDecl, this.compilerState.getSemanticInfoChain().getUnit(TypeScript.switchToForwardSlashes(fileName)));
+                        var signatureInfo = TypeScript.PullHelpers.getSignatureForFuncDecl(funcDecl, this.compilerState.getSemanticInfoChain().getUnit(fileName));
                         isCallExpression = true;
                         candidateSignature = signatureInfo.signature;
                         resolvedSignatures = signatureInfo.allSignatures;
