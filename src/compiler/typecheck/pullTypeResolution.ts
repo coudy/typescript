3eb31d17ff45c8b2d9ae90c0e753104cb919643b
Joe Pamer <jopamer@microsoft.com>
2013-11-05 17:00:51 -0800

    Remove code that had inner types being instantiated to 'any' during type inference.  It wasn't spec conformant, and could lead to invalid caching of instantiations. (The latter could result in heisenbugs.)

--- src/compiler/typecheck/pullTypeResolution.ts
+++ src/compiler/typecheck/pullTypeResolution.ts
@@ -10608,9 +10608,6 @@ module TypeScript {
             }
 
             if (parameterType.isTypeParameter()) {
-                if (expressionType.isGeneric() && !expressionType.isTypeParameter()) {
-                    expressionType = this.instantiateTypeToAny(expressionType, context);
-                }
                 argContext.addCandidateForInference(<PullTypeParameterSymbol>parameterType, expressionType, shouldFix);
                 return;
             }
