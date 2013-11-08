e31dbf3062492c44d2e9b28f3e5c70a580997582
Ryan Cavanaugh <ryanca@microsoft.com>
2013-11-08 09:45:05 -0800

    New regression tests

---
+++ tests/cases/fourslash/duplicateTypeParameters.ts
@@ -0,0 +1,8 @@
+/// <reference path="fourslash.ts" />
+
+//// class A<B, B/**/>  { }
+
+goTo.marker();
+// Bug 820926: Crash here
+// verify.quickInfoExists();
+
---
+++ tests/cases/fourslash/moduleEnumModule.ts
@@ -0,0 +1,15 @@
+/// <reference path="fourslash.ts" />
+
+////module A {
+////    var o;
+////}
+////enum A {
+////    /**/c
+////}
+////module A {
+////    var p;
+////}
+
+goTo.marker();
+// Bug 820916: Crash here
+// verify.quickInfoExists();
