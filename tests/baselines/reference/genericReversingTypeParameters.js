ffc7f44c95cbe0b926bef0320dc62ddd6fdd6d1a
Dan Quirk <danquirk@microsoft.com>
2013-11-15 14:25:32 -0800

    Adding tests for resolved bugs

---
+++ tests/baselines/reference/genericReversingTypeParameters.js
@@ -0,0 +1,16 @@
+var BiMap = (function () {
+    function BiMap() {
+    }
+    BiMap.prototype.get = function (key) {
+        return null;
+    };
+    BiMap.prototype.inverse = function () {
+        return null;
+    };
+    return BiMap;
+})();
+
+var b = new BiMap();
+var r1 = b.get('');
+var i = b.inverse();
+var r2b = i.get(1);
---
+++ tests/baselines/reference/genericReversingTypeParameters2.js
@@ -0,0 +1,15 @@
+var BiMap = (function () {
+    function BiMap() {
+    }
+    BiMap.prototype.get = function (key) {
+        return null;
+    };
+    BiMap.prototype.inverse = function () {
+        return null;
+    };
+    return BiMap;
+})();
+
+var b = new BiMap();
+var i = b.inverse();
+var r2b = i.get(1);
