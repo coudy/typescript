66b8561f3e6c8e2cc29ad5dfa70eb86d54f8d758
Dan Quirk <danquirk@microsoft.com>
2014-01-27 11:15:34 -0800

    Some test updates for promises

--- tests/baselines/reference/propagationOfPromiseInitialization.js
+++ tests/baselines/reference/propagationOfPromiseInitialization.js
@@ -1,11 +1,9 @@
 var foo;
-
 foo.then(function (x) {
     // x is inferred to be a number
     return "asdf";
 }).then(function (x) {
     // x is inferred to be string
     x.length;
-
     return 123;
 });
